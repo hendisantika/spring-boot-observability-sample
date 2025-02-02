615fa06962c7b26baea565dd0e7877819bc6c3f6
hendisantika <hendisantika@yahoo.co.id>
2025-02-02 13:43:35 +0700

    feat: Add PrometheusExemplarSamplerConfiguration 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/observability/config/PrometheusExemplarSamplerConfiguration.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.observability.config;
+
+import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-observability-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/02/25
+ * Time: 13.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class PrometheusExemplarSamplerConfiguration {
+    @Bean
+    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
+        return new OpenTelemetryAgentSpanContextSupplier();
+    }
+}
