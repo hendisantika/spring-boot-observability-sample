bb3375bcc759f1e51f67b5ef810d650530863c7b
hendisantika <hendisantika@yahoo.co.id>
2025-02-02 13:36:58 +0700

    feat: Init Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/observability/SpringBootObservabilitySampleApplication.java
@@ -0,0 +1,13 @@
+package observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootObservabilitySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootObservabilitySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/observability/SpringBootObservabilitySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootObservabilitySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
