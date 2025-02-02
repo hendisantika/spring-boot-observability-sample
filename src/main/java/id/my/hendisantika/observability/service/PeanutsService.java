91e6ef90e5a7138f176a93e414b7e723d880e155
hendisantika <hendisantika@yahoo.co.id>
2025-02-02 13:44:36 +0700

    feat: Add getPeanutsById in PeanutsService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/observability/service/PeanutsService.java
+++ src/main/java/id/my/hendisantika/observability/service/PeanutsService.java
@@ -1,7 +1,9 @@
 package id.my.hendisantika.observability.service;
 
+import id.my.hendisantika.observability.entity.Peanuts;
 import id.my.hendisantika.observability.repository.PeanutsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,6 +20,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PeanutsService {
-    private final PeanutsRepository repository;
+    private final PeanutsRepository peanutsRepository;
+
+    @Cacheable(value = "peanuts", key = "#id")
+    public Peanuts getPeanutsById(Long id) {
+        return peanutsRepository.findById(id).orElse(null);
+    }
+
 
 }
