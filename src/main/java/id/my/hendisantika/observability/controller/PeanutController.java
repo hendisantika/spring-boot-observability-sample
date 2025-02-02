17764556c11eaead216bb5d73fdd64afaa00d6c5
hendisantika <hendisantika@yahoo.co.id>
2025-02-02 13:46:43 +0700

    feat: Add root in PeanutController 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/observability/controller/PeanutController.java
+++ src/main/java/id/my/hendisantika/observability/controller/PeanutController.java
@@ -2,8 +2,13 @@ package id.my.hendisantika.observability.controller;
 
 import id.my.hendisantika.observability.service.PeanutsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 13.45
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Service
 @EnableCaching
 @RestController
@@ -24,4 +30,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PeanutController {
     private final PeanutsService peanutsService;
 
+    @GetMapping("/")
+    public String root(@RequestParam(value = "name", defaultValue = "World") String name, @RequestHeader HttpHeaders headers) {
+        log.error(headers.toString());
+        log.error("Hello {}!!", name);
+        log.debug("Debugging log");
+        log.info("Info log");
+        log.warn("Hey, This is a warning!");
+        log.error("Oops! We have an Error. OK");
+        return String.format("Hello %s!!", name);
+    }
 }
