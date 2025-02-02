fc33d041d25fb37ff450437a0a9a8102171a27b4
hendisantika <hendisantika@yahoo.co.id>
2025-02-02 13:49:18 +0700

    feat: Add savePeanuts in PeanutController 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/observability/controller/PeanutController.java
+++ src/main/java/id/my/hendisantika/observability/controller/PeanutController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -109,4 +111,9 @@ public class PeanutController {
         return peanutsService.getPeanutsById(id);
     }
 
+    @PostMapping("/peanuts")
+    public Peanuts savePeanuts(@RequestBody Peanuts peanuts) {
+        log.info("Create Peanuts Character");
+        return peanutsService.savePeanuts(peanuts);
+    }
 }
